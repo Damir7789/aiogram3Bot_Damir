15ce525d1d957571f8facaedc2768f70c530759e
Damir <145147112+Damir7789@users.noreply.github.com>
2023-10-25 16:00:12 +0500

    Добавлено mute по времени

--- aioBot.py
+++ aioBot.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+import datetime
 from time import sleep
 # from datetime import datetime
 
@@ -7,13 +8,12 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command, CommandObject
 from aiogram.types import FSInputFile
 from random import randint
+from aiogram.types import ChatPermissions
 
 TELEGRAM_TOKEN = "6263696820:AAGfWdezicdY9eLKp-uM_UjHfqOjht-dIa8"
-# moya gruppa
-# GROUP_ID = "-1002100115467"
+
 # test grp
 GROUP_ID = "-1001674247269"
-
 
 # вывод отладочних сообщений в терминал
 logging.basicConfig(level=logging.INFO)
@@ -34,7 +34,7 @@ async def upload_forto(message: types.Message):
 
 @dp.message(Command("mygroup"))
 async def cmd_to_group(message: types.Message, bot: Bot):
-    await bot.send_message(GROUP_ID, "Hello from Damir")
+    await bot.send_message(message.chat.id, "Hello from Damir")
 
 # обработка бработка команды рандом
 # /rand
@@ -47,7 +47,7 @@ async def get_random(message: types.Message, command: CommandObject):
 # команда бан    
 @dp.message(Command(commands=["ban"]))
 async def zabanit(message: types.Message):
-    # user_status = await message.bot.get_chat_member(chat_id=GROUP_ID, user_id=message.from_user.id)
+    # user_status = await message.bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
     user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id) 
     if isinstance(user_status, types.chat_member_owner.ChatMemberOwner) or isinstance(user_status, types.chat_member_administrator.ChatMemberAdministrator):
         print ("\n\n Owner not Admin - TRUE \n\n")
@@ -60,15 +60,30 @@ async def zabanit(message: types.Message):
         await message.reply("Пиши команду ban в ответ на сообщение")
         return 
     banned_user = message.reply_to_message.from_user.first_name
-    await message.bot.ban_chat_member(chat_id=GROUP_ID, user_id=message.reply_to_message.from_user.id)
-    await message.bot.delete_message(chat_id=GROUP_ID, message_id=message.message_id)
+    await message.bot.ban_chat_member(chat_id=message.chat.id, user_id=message.reply_to_message.from_user.id)
+    await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     await message.reply_to_message.reply(f'Пользователь {banned_user} забанен')
+
+# mute 
+@dp.message(Command(commands=["mute"]))
+async def mute(message: types.Message, command: CommandObject, bot:Bot):
+    userADMIN = message.from_user.first_name
+    userID = message.reply_to_message.from_user.id
+    userNAME = message.reply_to_message.from_user.first_name
+    # kak_dolgo = 3
+    # разбываем аргументи на long и kak dolgo
+    long, kak_dolgo = [n for n in command.args.split("-")]
+    kak_dolgo = int(kak_dolgo)
+    vremya = datetime.timedelta(hours = kak_dolgo)
+    await message.bot.restrict_chat_member(chat_id=message.chat.id, user_id=userID, permissions=ChatPermissions(can_send_messages=False), until_date=vremya)
+    await message.reply(f"{userADMIN} замутил {userNAME} на {kak_dolgo} часа !!!!!")
     
 # ping pong
 @dp.message()
 async def echo(message: types.Message):
     print("message listened")
     # await message.answer("Бот Дамира услышал от, {0.first_name} : " + message.text)
+
 
 # непреривный режим работы бота
 async def main():
